058cd98119d7483767669290124418e92b4e34da
Peace Walker <baskakovforjob@gmail.com>
2019-07-30 22:48:16 +0300

    added key controls + some logic

--- C&C++/console_tetris/c_tetris.cpp
+++ C&C++/console_tetris/c_tetris.cpp
@@ -1,7 +1,10 @@
+#include <termios.h>
 #include <iostream>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+
 
 using namespace std;
 
@@ -13,7 +16,30 @@ const int TETRIS_BLOCK_BASE_SIZE = 4;
 const char GAME_FIELD_BORDER = '#';
 const char BLOCK_STYLE = '@';
 
-enum TETRIS_BLOCK{I_BLOCK, J_BLOCK, L_BLOCK, O_BLOCK, S_BLOCK, T_BLOCK, Z_BLOCK};
+enum TETRIS_BLOCK{I_BLOCK = 0, J_BLOCK, L_BLOCK, O_BLOCK, S_BLOCK, T_BLOCK, Z_BLOCK};
+//enum INPUT_KEY{KEY_UP = 72, KEY_DOWN = 80, KEY_LEFT = 75, KEY_RIGHT = 77};
+
+
+char getch(void){
+    char buf = 0;
+    struct termios old = {0};
+    fflush(stdout);
+    if(tcgetattr(0, &old) < 0)
+        perror("tcsetattr()");
+    old.c_lflag &= ~ICANON;
+    old.c_lflag &= ~ECHO;
+    old.c_cc[VMIN] = 1;
+    old.c_cc[VTIME] = 0;
+    if(tcsetattr(0, TCSANOW, &old) < 0)
+        perror("tcsetattr ICANON");
+    if(read(0, &buf, 1) < 0)
+        perror("read()");
+    old.c_lflag |= ICANON;
+    old.c_lflag |= ECHO;
+    if(tcsetattr(0, TCSADRAIN, &old) < 0)
+        perror("tcsetattr ~ICANON");
+    return buf;
+ }
 
 class tetris_block{
     public: 
@@ -135,6 +161,9 @@ class tetris_block{
 class console_tetris{
     public:
         console_tetris(){
+
+            srand(time(NULL));
+
             I_block = new tetris_block(I_BLOCK);
             J_block = new tetris_block(J_BLOCK);
             L_block = new tetris_block(L_BLOCK);
@@ -142,12 +171,16 @@ class console_tetris{
             S_block = new tetris_block(S_BLOCK);
             T_block = new tetris_block(T_BLOCK);
             Z_block = new tetris_block(Z_BLOCK);
-            
-            gravity_stop_line = 0;
+
+            CURRENT_block = I_block;
+            gravity_stop_line = GAME_FIELD_HEIGHT - 1;
             falling_block_exists_on_game_field = false;
+            block_pos_X = 1;
+            block_pos_Y = 1;
 
             create_game_field();
             fill_game_field_with_border();
+            draw_game_field();
         }
         ~console_tetris(){
             free(I_block);
@@ -157,9 +190,22 @@ class console_tetris{
             free(S_block);
             free(T_block);
             free(Z_block);
+            free(CURRENT_block);
         }
 /////////////////////////////////////////////////////////
     public:
+
+        void run_game(){
+            while(true){
+            clear_game_field();
+            react_on_key_input(CURRENT_block); 
+            draw_game_field();
+            printf("[X]: %d ", block_pos_X);
+            printf("[Y]: %d\n", block_pos_Y);
+            usleep(50 * 1000);
+    }
+        }
+
         bool draw_tetris_block(int x_head, int y_head, int x_tail, int y_tail, TETRIS_BLOCK requested_block){
 
             falling_block_exists_on_game_field = true;
@@ -203,16 +249,106 @@ class console_tetris{
             } 
         }
 /////////////////////////////////////////////////////////
-    protected:
-        void involve_gravity(){
-            
+    public:
 
+        void react_on_key_input(tetris_block* requested_block){
+
+            switch(getch()){
+                case 'w':
+                    fix_block_position__get_new_block(--block_pos_X, block_pos_Y, 0, 0, requested_block);
+                break;
+
+                case 'd':
+                    fix_block_position__get_new_block(block_pos_X, ++block_pos_Y, 0, 0, requested_block);
+                break;
+
+                case 'a':
+                    fix_block_position__get_new_block(block_pos_X, --block_pos_Y, 0, 0, requested_block);
+                break;
+
+                case 's':
+                    fix_block_position__get_new_block(++block_pos_X, block_pos_Y, 0, 0, requested_block);
+                break;
+            }
+        }
+
+        void clear_game_field(){
+            for(int x = 1; x < GAME_FIELD_HEIGHT - 1; x++)
+                for(int y = 1; y < GAME_FIELD_WIDTH - 1; y++){
+                    if(game_field[x][y] == BLOCK_STYLE) 
+                    game_field[x][y] = ' ';       
+                }
+        }
+
+        void involve_gravity(){
+                
+        }
+
+        void fix_block_position__get_new_block(int x_head, int y_head, int x_tail, int y_tail, tetris_block* requested_block){
+            
+            int block_tail = block_pos_X + requested_block->b_height;
+            cout << block_tail << endl;
+
+            if(block_tail == GAME_FIELD_HEIGHT - 2){
+                gravity_stop_line = block_pos_X;
+                block_pos_X = 0;
+                block_pos_Y = 0;
+                CURRENT_block = get_random_block();
+
+                paste_block_on_game_field(x_head, y_head, x_tail, y_tail, CURRENT_block);
+            }else
+                paste_block_on_game_field(x_head, y_head, x_tail, y_tail, CURRENT_block);        
+        }
+
+        tetris_block* get_random_block(){
+            int rand_block = rand() % TETRIS_BLOCK_COUNT;
+            return parse_block_by_number(rand_block);
+        }
+
+        tetris_block* parse_block_by_number(int block_number){
+            switch (block_number){
+                case 0:
+                    return I_block;
+                break;
+                    
+                case 1:
+                    return J_block;
+                break;
+
+                case 2:
+                    return L_block;
+                break;
+
+                case 3:
+                    return O_block;
+                break;
+
+                case 4:
+                    return S_block;
+                break;
+
+                case 5:
+                    return T_block;
+                break;
+
+                case 6:
+                    return Z_block;
+                break;
+            }
         }
 
         void paste_block_on_game_field(int x_head, int y_head, int x_tail, int y_tail, tetris_block *requested_block){
+            
+            x_tail = x_head + 1 + requested_block->b_height;
+            y_tail = y_head + 1 + requested_block->b_width;
+
             for(int paste_x = x_head + 1, t_block_index_x = 0; t_block_index_x < requested_block->b_height; paste_x++, t_block_index_x++)
-                for(int paste_y = y_head + 1, t_block_index_y = 0; t_block_index_y < requested_block->b_width; paste_y++, t_block_index_y++)
-                    game_field[paste_x][paste_y] = requested_block->t_block[t_block_index_x][t_block_index_y];                         
+                for(int paste_y = y_head + 1, t_block_index_y = 0; t_block_index_y < requested_block->b_width; paste_y++, t_block_index_y++){
+                    if((x_tail < GAME_FIELD_HEIGHT) && (x_head + 1 > 0) && (y_tail < GAME_FIELD_WIDTH) && (y_head + 1 > 0))
+                        game_field[paste_x][paste_y] = requested_block->t_block[t_block_index_x][t_block_index_y];                         
+                    else 
+                        printf("[!]OUT OF BORDER[!]");
+                } 
         }
 
         bool fill_game_field_with_border(){
@@ -240,6 +376,8 @@ class console_tetris{
         char game_field[GAME_FIELD_HEIGHT][GAME_FIELD_WIDTH];
         bool falling_block_exists_on_game_field;
         int gravity_stop_line;
+        int block_pos_X;
+        int block_pos_Y;
         tetris_block* I_block;
         tetris_block* J_block;
         tetris_block* L_block;
@@ -247,6 +385,7 @@ class console_tetris{
         tetris_block* S_block;
         tetris_block* T_block;
         tetris_block* Z_block;
+        tetris_block* CURRENT_block;
 
 };
 
@@ -254,8 +393,9 @@ int main()
 {
     console_tetris ct;
 
-    ct.draw_tetris_block(1, 1, 0, 0, L_BLOCK);
-    ct.draw_game_field();
+    ct.run_game();
+   
+    
 
     
     return 0;
