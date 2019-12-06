6ec56fa5668624e3d3b9d63976a0177322355bd8
Peace Walker <baskakovforjob@gmail.com>
2019-12-06 23:12:49 +0200

    communication between client and server

--- py_game_project/game_client.py
+++ py_game_project/game_client.py
@@ -6,16 +6,17 @@ import pygame
 
 from socket import *
 
-client_ip = "192.168.0.104"
-client_recieve_port = 4000
+client_ip = ""
+client_recieve_port = 4031
+client_data_buffer = 4096
 
 client_address = (client_ip, client_recieve_port)
-
+print(client_address)
 UDPSocket = socket(AF_INET, SOCK_DGRAM)
 UDPSocket.bind(client_address)
 
 server_ip = "192.168.0.108"
-server_recieve_port = 1500
+server_recieve_port = 1546
 
 server_address = (server_ip, server_recieve_port)
 
@@ -31,14 +32,29 @@ def Connect_to_server():
     global PLAYER_INFO
     print("[...]Connecting to server[...]")
     UDPSocket.sendto(bytes("HELLO", "utf-8"), server_address)
-    pygame.time.delay(16)
     for i in range (len(PLAYER_INFO)):
         print(PLAYER_INFO[i])
         UDPSocket.sendto(bytes([PLAYER_INFO[i]]), server_address)
     print("[!]Connect successfull[!]")
 
+def Check_status():
+    print("RECV_STATUS")
+    (recieved_data, incoming_ip) = UDPSocket.recvfrom(client_data_buffer)
+    recieved_data = bool(recieved_data)
+    if(recieved_data):
+        print("WAIT_STATUS_RECIEVED")
+    else:
+        print("STILL_RUNNING...")
+    print("CHECK_STATUS")
+
+def Send_player_position():
+    global PLAYER_INFO
+    UDPSocket.sendto(bytes([PLAYER_X]), server_address)
+    UDPSocket.sendto(bytes([PLAYER_Y]), server_address)
 
 Connect_to_server()
+Check_status()
+
 
 while True:
     data = bytes(input("Enter message to send or type 'exit': "), "utf-8")
--- py_game_project/game_server.py
+++ py_game_project/game_server.py
@@ -10,18 +10,32 @@ SERVER_IS_RUNNING = True
 server_shutdown_command = ""
 
 server_host_ip = ""
-server_recieve_port = 1500
+server_recieve_port = 1546
 server_data_buffer = 4096
 
 server_address = (server_host_ip, server_recieve_port)
+
 
 UDPSocket = socket(AF_INET, SOCK_DGRAM)
 UDPSocket.bind(server_address)
 ##########################################################
 PLAYERS_COUNT = 2
 
-PLAYER_1_MACHINE_ADDRESS = []
-PLAYER_2_MACHINE_ADDRESS = []
+PLAYER_1_MACHINE = []
+PLAYER_1_RECIEVE_PORT = 4030
+PLAYER_1_ADDRESS = ()
+
+PLAYER_1_WAITING_STATUS = False
+
+##########################################################
+
+PLAYER_2_MACHINE = []
+PLAYER_2_RECIEVE_PORT = 4031
+PLAYER_2_ADDRESS = ()
+
+PLAYER_2_WAITING_STATUS = False
+
+##########################################################
 
 PLAYER_1_INFO = PLAYER_1_X, PLAYER_1_Y, PLAYER_1_W, PLAYER_1_H, PLAYER_1_CUBE_COLOR_R, PLAYER_1_CUBE_COLOR_G, PLAYER_1_CUBE_COLOR_B = 0, 0, 0, 0, 0, 0, 0
 PLAYER_2_INFO = PLAYER_2_X, PLAYER_2_Y, PLAYER_2_W, PLAYER_2_H, PLAYER_2_CUBE_COLOR_R, PLAYER_2_CUBE_COLOR_G, PLAYER_2_CUBE_COLOR_B = 0, 0, 0, 0, 0, 0, 0
@@ -31,51 +45,48 @@ PLAYER_2_POSITION = PLAYER_2_X, PLAYER_2_Y
 
 PLAYER_1_CUBE_COLOR = 255, 255, 255
 PLAYER_2_CUBE_COLOR = 255, 255, 255
-
-# PLAYER_1_X = 0
-# PLAYER_1_Y = 0
-# PLAYER_1_W = 0
-# PLAYER_1_H = 0
-
-# PLAYER_2_X = 0
-# PLAYER_2_Y = 0
-# PLAYER_2_W = 0
-# PLAYER_2_H = 0
 ##########################################################
 
 
 def Get_player_address():
-    global PLAYER_1_MACHINE_ADDRESS
-    global PLAYER_2_MACHINE_ADDRESS
+    global PLAYER_1_MACHINE
+    global PLAYER_2_MACHINE
+    global PLAYER_1_ADDRESS
+    global PLAYER_2_ADDRESS
+
     global PLAYER_1_INFO
     global PLAYER_2_INFO
     while 1:
-        # if(len(PLAYER_1_MACHINE_ADDRESS) > 0 & len(PLAYER_2_MACHINE_ADDRESS) > 0):
+        # if(len(PLAYER_1_MACHINE) > 0 & len(PLAYER_2_MACHINE
+        # ) > 0):
         #     print("[!]All players has been connected[!]")
         #     break
-        if(len(PLAYER_1_MACHINE_ADDRESS) == 0):
+        if(len(PLAYER_1_MACHINE) == 0):
             print("[...]Waiting Player 1 to connect[...]")
             (recieved_data, client_address) = UDPSocket.recvfrom(server_data_buffer)
-            PLAYER_1_MACHINE_ADDRESS = client_address
+            PLAYER_1_MACHINE = client_address
+            PLAYER_1_ADDRESS = str(PLAYER_1_MACHINE[0]), PLAYER_1_RECIEVE_PORT
             Get_player_info(1)
-            print(PLAYER_1_INFO)
+            print(PLAYER_1_ADDRESS)
             print("[*]Player 1 has been connected[*]")
-            print("Player 1 ID: " + str(PLAYER_1_MACHINE_ADDRESS[1]))
+            print("Player 1 ID: " + str(PLAYER_1_MACHINE[1]))
         else:
             print("[...]Waiting Player 2 to connect[...]")
             (recieved_data, client_address) = UDPSocket.recvfrom(server_data_buffer)
-            if(client_address[1] != PLAYER_1_MACHINE_ADDRESS[1]):
-                PLAYER_2_MACHINE_ADDRESS = client_address
+            if(client_address[1] != PLAYER_1_MACHINE[1]):
+                PLAYER_2_MACHINE = client_address
+                PLAYER_2_ADDRESS = str(
+                    PLAYER_2_MACHINE[0]), PLAYER_2_RECIEVE_PORT
                 Get_player_info(2)
-                print(PLAYER_2_INFO)
                 print("[*]Player 2 has been connected[*]")
-                print("Player 2 ID: " + str(PLAYER_2_MACHINE_ADDRESS[1]))
-        
+                print("Player 2 ID: " + str(PLAYER_2_MACHINE[1]))
+
                 print("[!]All players has been connected[!]")
                 break
     return None
 
-def Get_player_info(player_number):  
+
+def Get_player_info(player_number):
     global PLAYER_1_INFO
     global PLAYER_2_INFO
 
@@ -83,15 +94,16 @@ def Get_player_info(player_number):
     player_2_list = list(PLAYER_2_INFO)
 
     if(player_number == 1):
-        for i in range (len(player_1_list)):
+        for i in range(len(player_1_list)):
             (recieved_data, client_address) = UDPSocket.recvfrom(server_data_buffer)
             player_1_list[i] = int.from_bytes(recieved_data, "little")
         PLAYER_1_INFO = tuple(player_1_list)
-    if(player_number == 2): 
+    if(player_number == 2):
         for i in range(len(player_2_list)):
             (recieved_data, client_address) = UDPSocket.recvfrom(server_data_buffer)
             player_2_list[i] = int.from_bytes(recieved_data, "little")
         PLAYER_2_INFO = tuple(player_2_list)
+
 
 def Get_player_position(player_number):
     global PLAYER_1_INFO
@@ -107,6 +119,7 @@ def Get_player_position(player_number):
             (recieved_data, client_address) = UDPSocket.recvfrom(server_data_buffer)
             player_1_list[i] = int.from_bytes(recieved_data, "little")
         PLAYER_1_POSITION = tuple(player_1_list)
+        print(PLAYER_1_POSITION)
     if(player_number == 2):
         for i in range(len(player_2_list)):
             (recieved_data, client_address) = UDPSocket.recvfrom(server_data_buffer)
@@ -114,11 +127,31 @@ def Get_player_position(player_number):
         PLAYER_2_POSITION = tuple(player_2_list)
 
 
+def Set_client_status(player_number, status):
+    global PLAYER_1_WAITING_STATUS
+    global PLAYER_2_WAITING_STATUS
+
+    if(player_number == 1):
+        if(status == "wait"):
+            PLAYER_1_WAITING_STATUS = True
+        else:
+            PLAYER_1_WAITING_STATUS = False
+        print("t_send")    
+        UDPSocket.sendto(bytes(PLAYER_1_WAITING_STATUS), PLAYER_1_ADDRESS)
+        print("success_send")
+    else:
+        if(status == "wait"):
+            PLAYER_2_WAITING_STATUS = True
+        else:
+            PLAYER_2_WAITING_STATUS = False
+        UDPSocket.sendto(bytes(PLAYER_2_WAITING_STATUS), PLAYER_2_ADDRESS)
+
 Get_player_address()
+Set_client_status(1, "wait")
+Set_client_status(2, "sss")
 while 1:
+
     (recieved_data, client_address) = UDPSocket.recvfrom(server_data_buffer)
-    print(PLAYER_1_INFO)
-    print(PLAYER_2_INFO)
     recieved_data = str(recieved_data, "utf-8")
     if(recieved_data == "exit"):
         print("[!]SERVER HAS BEEN STOPED[!]")
